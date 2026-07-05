af21741afd32bd50702e0f030cb6d3278b201fa9
LuizFillip <luizfillip@outlook.com>
2026-07-05 13:45:32 -0300

    new functions for plot amplitude and phases

--- hamornic_phases.py
+++ hamornic_phases.py
@@ -24,7 +24,10 @@ def fit_harmonic_phase(t, y, period):
 
     beta, _, _, _ = np.linalg.lstsq(X, y, rcond=None)
     A, B, C = beta
-
+    
+    y_fit = X @ beta
+    res = y - y_fit
+    
     amplitude = np.sqrt(A**2 + B**2)
     phase_rad = np.arctan2(B, A)
 
@@ -33,21 +36,56 @@ def fit_harmonic_phase(t, y, period):
 
     # opcional: colocar no intervalo [0, period)
     phase_days_mod = phase_days % period
+    
+    n = len(y)
+    p = X.shape[1]          # = 3 parâmetros
+    
+    sigma2 = np.sum(res**2) / (n - p)
+    cov = sigma2 * np.linalg.inv(X.T @ X)
+    varA = cov[0, 0]
+    varB = cov[1, 1]
+    varC = cov[2, 2]
+    
+    covAB = cov[0, 1]
+    amplitude_std = np.sqrt(
+            (A/amplitude)**2 * varA +
+            (B/amplitude)**2 * varB +
+            2*A*B/amplitude**2 * covAB
+        )
+    
+    dphi_dA = -B / (A**2 + B**2)
+    dphi_dB =  A / (A**2 + B**2)
+    
+    phase_std_rad = np.sqrt(
+        dphi_dA**2 * varA +
+        dphi_dB**2 * varB +
+        2*dphi_dA*dphi_dB*covAB
+    )
+    
+    phase_std_days = phase_std_rad * period / (2*np.pi)
 
-    y_fit = X @ beta
 
     return {
         'A': A,
         'B': B,
         'C': C,
+    
         'amplitude': amplitude,
+        'amplitude_std': amplitude_std,
+    
         'phase_rad': phase_rad,
+        'phase_std_rad': phase_std_rad,
+    
         'phase_days': phase_days,
+        'phase_std_days': phase_std_days,
+    
         'phase_days_mod': phase_days_mod,
-        'y_fit': y_fit,
-        't_fit': t
+    
+        # 'y_fit': y_fit,
+        # 't_fit': t,
+    
+        # 'covariance': cov
     }
-
 def vertical_phase_parameters(alt_km, phase_days, period):
     alt_km = np.asarray(alt_km, dtype=float)
     phase_days = np.asarray(phase_days, dtype=float)
@@ -56,25 +94,48 @@ def vertical_phase_parameters(alt_km, phase_days, period):
     alt_km = alt_km[idx]
     phase_days = phase_days[idx]
 
-    slope, intercept = np.polyfit(alt_km, phase_days, 1)
+ 
 
-    # comprimento de onda vertical
+    coef, cov = np.polyfit(
+        alt_km,
+        phase_days,
+        1,
+        cov=True
+    )
+    
+    slope, intercept = coef
+    
+    slope_std = np.sqrt(cov[0,0])
+    intercept_std = np.sqrt(cov[1,1])
+    
     lambda_z_km = period / abs(slope)
-
-    # velocidade vertical de fase
-    vz_km_day = 1.0 / slope
+    lambda_z_std = period * slope_std / (abs(slope)**2)
+    
+    vz_km_day = 1/slope
+    vz_km_day_std = slope_std / (slope**2)
+    
     vz_m_s = vz_km_day * 1000 / 86400
-
+    vz_m_s_std = vz_km_day_std * 1000 / 86400
+    
     phase_fit = slope * alt_km + intercept
-
+    
     return {
         "slope_days_per_km": slope,
+        "slope_std": slope_std,
+    
         "intercept_days": intercept,
+        "intercept_std": intercept_std,
+    
         "lambda_z_km": lambda_z_km,
+        "lambda_z_std": lambda_z_std,
+    
         "vz_km_day": vz_km_day,
+        "vz_km_day_std": vz_km_day_std,
+    
         "vz_m_s": vz_m_s,
-        "phase_fit": phase_fit,
-        "alt_km": alt_km
+        "vz_m_s_std": vz_m_s_std, 
+        
+        'phase_fit': phase_fit
     }
 
 
--- phase_propagation_saber.py
+++ phase_propagation_saber.py
@@ -2,13 +2,17 @@ import SABER as sb
 import pandas as pd 
 import matplotlib.pyplot as plt 
 import numpy as np 
-import waves as wv
 from tqdm import tqdm 
+import waves as wv
+import base as b 
+b.sci_format()
 
 def join_heights(
         df_main, alts,
         bandpass = (3, 15),
-        lat_center = -7, ref_lon = -35):
+        lat_center = -7, 
+        ref_lon = -35
+        ):
     out = []
     desc = 'Selecting Layers'
     for alt in tqdm(alts, desc):
@@ -17,6 +21,7 @@ def join_heights(
                 f'temp_{alt}', 
                 lat_center = lat_center,
                 bandpass = bandpass,
+                normalize = False
                
                 )
         ds = df.loc[df.index == ref_lon].T
@@ -27,6 +32,7 @@ def join_heights(
 
 
  
+
 
 
 def get_phases_mod(ds, period = 13, unwrap = True):
@@ -43,51 +49,119 @@ def get_phases_mod(ds, period = 13, unwrap = True):
     else:
         return phases 
 
+args = dict(
+    linestyle = 'none',
+    fillstyle = 'none',
+    marker = 'o',
+    markersize = 20,
+    capsize = 5
+    )
+   
 def plot_heights_vs_phase(
-        ax, phase_days, altitudes, period):
-    res = wv.vertical_phase_parameters(altitudes, phase_days, period)
+        ax, ds, period
+        ):
+    
+    res = wv.vertical_phase_parameters(
+        ds.index, ds['phase_days_mod'].values, period)
 
     vz_ms = res['vz_m_s']
     lambda_z = res['vz_km_day'] * period
     
     infos = (
-             f'T = {period} days\n' + 
-             f'Vz = {vz_ms:.2f} m/s\n' + 
-             f'$\lambda_z$ = {lambda_z:.2f} km')
-    
-
+        f'T = {period} days\n' + 
+        f'Vz = {vz_ms:.2f} m/s\n' + 
+        f'$\lambda_z$ = {lambda_z:.2f} km'
+        )
    
-    ax.scatter(phase_days, altitudes, marker = 'o', s = 100)
-    ax.plot( res['phase_fit'], altitudes, '-', lw = 3, color = 'red')
+ 
+   
+    ax.errorbar(
+        ds['phase_days_mod'], 
+        ds.index,
+        xerr = ds['phase_std_days'], 
+        color = 'red', 
+        **args 
+        )
+    
+    phase_max = ds['phase_days_mod'].max() + 2
+    phase_max = round(phase_max)
+    ax.plot( res['phase_fit'], ds.index, '-', lw = 3, color = 'red')
     ax.set(
            ylabel = 'Altitude (km)', 
            xlabel = 'Phase (days)', 
-           ylim = [0, 110],
-           xlim = [-4, 8]
+           ylim = [10, 110],
+           xlim = [-phase_max, phase_max],
+           xticks = np.arange(-phase_max, phase_max, 2)
            )
     
     ax.text(
-        0.45, 0.01, 
+        0.05, 0.8, 
         infos, 
-        fontsize = 20,
+        fontsize = 25,
         transform = ax.transAxes
         )
+
+def plot_heights_vs_amplitude(ax, ds, color = 'blue'):
+    
+    ax.errorbar(
+        ds['amplitude'], 
+        ds.index,
+        xerr = ds['amplitude_std'], 
+        color = color, 
+        **args 
+        )
+    
+    amplitude_max = ds['amplitude'].max() *1.2
+    ax.set( 
+        xlabel = 'Amplitude (K)', 
+        ylim = [10, 110],
+        xlim = [-0.2, amplitude_max], 
+        )
+     
+    coef, cov = np.polyfit(
+        ds.index,
+        ds['amplitude'],
+        1,
+        cov=True
+    )
+    
+    slope, intercept = coef
+    
+    slope_std = np.sqrt(cov[0,0])
+    intercept_std = np.sqrt(cov[1,1])
+    
+    amplitude_fit = slope * ds.index + intercept
+    
+    ax.plot(amplitude_fit, ds.index, '-', lw = 3, color = color)
+    
+    print(slope)
+    
+    
+def plot_phase_amplitude_propagations(ds, period):
+    fig, ax = plt.subplots(
+         ncols = 2,
+         sharey = True,
+         figsize= (14, 7), 
+         dpi = 300
+         )
+    
+    plt.subplots_adjust(wspace = 0.1)
+    plot_heights_vs_phase(ax[0], ds, period)
+    plot_heights_vs_amplitude(ax[1], ds)
+    
+    return fig, ax
 
 df_main = sb.saber_data( )
  
 alts = np.arange(20, 110, 10)
- 
-ds_all = join_heights(df_main, alts, ref_lon = -35)
+lat_center = 7
+ds_all = join_heights(
+    df_main, alts,
+    lat_center = lat_center, ref_lon = -35)
  
 
-#%%%
+ 
 
-fig, ax = plt.subplots(
-     ncols = 2,
-     sharey = True,
-     figsize= (12, 10), 
-     dpi = 300
-     )
 
 ds = ds_all.loc[
     (ds_all.index >= 50) & 
@@ -97,15 +171,24 @@ ds = ds_all.loc[
  
 period = 5
  
-phases = get_phases_mod(ds, period = period)
 
-plot_heights_vs_phase(ax[0], phases, alts, period)
+phases = []
+altitudes = ds.columns
 
-def plot_fluctuations_by_height(ax, ds):
-    for alt in ds.columns:
-        ax.plot(ds[alt] + alt)
+for col in altitudes:
+
+    fit = wv.fit_harmonic_phase(ds.index, ds[col], period)
+    phases.append(pd.DataFrame(fit, index = [col]) )
     
-    ax.set(xlabel = 'Day of year')
-        
+df = pd.concat(phases)
+
+df['phase_days_mod'] = wv.unwrap_phase_days(
+    df['phase_days_mod'], 
+    period = period
+    )
     
-plot_fluctuations_by_height(ax[1], ds)
+df 
+
+fig, ax = plot_phase_amplitude_propagations(df, period)
+
+ax.set(title = f'Center latitude {lat_center}°')
