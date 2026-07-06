abd2187312fc881a988febefe5fc91560fbf1174
LuizFillip <luizfillip@outlook.com>
2026-07-06 17:32:07 -0300

    updates

--- __init__.py
+++ __init__.py
@@ -1,2 +1,3 @@
 from .synthetic_wave_grid import create_synthetic_wave_grid
 from .hamornic_phases import * 
+from .phase_propagation_saber import join_heights, filter_interval_and_fitting_data
--- wavenumber_frequency_decomposition.py
+++ wavenumber_frequency_decomposition.py
@@ -9,15 +9,20 @@ import base as b
 df = sb.saber_data()
  
 value_col = "temp_90"
-
+lat_center = -
 ds = sb.box_groupy_process(
         df, 
         value_col, 
-        lat_center = -7, 
-        bandpass = (2.1, 20) 
+        lat_center = lat_center, 
+        bandpass = (2.2, 15),
+        lon_step = 20,
+        lat_step = 20,
+        lon_stride = None
         )
  
- 
+
+
+
 def hanning_remove_tendency(T):
     
     # T = T - np.nanmean(T)
@@ -32,26 +37,27 @@ def hanning_remove_tendency(T):
     T = T * wlon * wtime
     return T
  
-def zonal_propagation(ds, period_min=3, period_max=20, smax=None):
+def zonal_propagation(ds, period_min=3, period_max=20):
     lon = ds.index.values.astype(float)
     doy = ds.columns.values.astype(float)
   
     T =  hanning_remove_tendency(ds.values.astype(float))
+    # T = ds.values.astype(float)
      
-    dlon = (lon[1] - lon[0]) / 360.0
-    dt = doy[1] - doy[0]
+    # dlon = (lon[1] - lon[0]) / 360.0
+    
+    dlon = np.nanmedian(np.diff(lon)) / 360.0
+    dt = np.nanmedian(np.diff(doy))
+    
+    print(lon[:5])
+    print("dlon =", dlon)
+    # dt = doy[1] - doy[0]
 
     F = np.fft.fft2(T)
 
     power = np.abs(F)**2
 
-    # remove explicitamente o modo k=0
-    # s_raw = np.fft.fftfreq(len(lon), d=dlon)
-    # idx_k0 = np.argmin(np.abs(s_raw))
-    # power[idx_k0, :] = np.nan
-
-    # normalização antes do log
-    #
+ 
     power = np.abs( np.log10(power)) 
     # power = power / np.nanmax(power)
     
@@ -61,7 +67,11 @@ def zonal_propagation(ds, period_min=3, period_max=20, smax=None):
     s_shift = np.fft.fftshift(s_shift)
     power_shift = np.fft.fftshift(power, axes=0)
 
-    idx_f = freq >= 0
+    # idx_f = freq >= 0
+    # freq_pos = freq[idx_f]
+    # period_pos = 1 / freq_pos
+    
+    idx_f = np.where(freq > 0)[0]   # exclui freq = 0
     freq_pos = freq[idx_f]
     period_pos = 1 / freq_pos
     power_pos = power_shift[:, idx_f]
@@ -72,14 +82,24 @@ def zonal_propagation(ds, period_min=3, period_max=20, smax=None):
     power_pos = power_pos[:, idx_p]
 
     S, P = np.meshgrid(s_shift, period_pos, indexing="ij")
+    
+
     return  S, P, power_pos
+
  
-def plot_zonalnumber_decomposition( ax,  ds ):
+def plot_zonalnumber_decomposition(
+        ax,  ds,
+        period_min = 3, 
+        period_max = 15
+        ):
     
     
     S,  P, power_pos = zonal_propagation(
-        ds, period_min=2, period_max=15, smax=None)
- 
+        ds, period_min=period_min, 
+        period_max= period_max
+        )
+    
+    # print(P)
     img = ax.contourf(
         S,  P, power_pos,
         levels=50,
@@ -104,14 +124,15 @@ def plot_zonalnumber_decomposition( ax,  ds ):
         fontsize=fontsize, color="k"
         )
 
-    for p in [5, 10]:
+    for p in [5, 11]:
         ax.axhline(p, linestyle = ":", color="k", linewidth=1)
         
-    ax.axvline(-1, linestyle ='--')
+    ax.axvline(-4, linestyle ='--')
     ax.set(
         xlabel="Zonal wave number",
         ylabel="Period (days)",
-        # ylim=(period_min, period_max)
+        # xlim = (-20, 20),
+        ylim=(period_min, period_max)
     )
 
 
@@ -130,7 +151,7 @@ def plot_zonalnumber_decomposition( ax,  ds ):
    
     return None
 
-#%%%
+
 
 
 import pandas as pd 
@@ -145,25 +166,25 @@ fig, ax = plt.subplots(
 plot_zonalnumber_decomposition( ax[1], ds )
 
 
-ds1 = pd.DataFrame(
-     gaussian_filter( 
-         ds.to_numpy(), 
-         sigma = 0.5
-         ),
-     index = ds.index,
-     columns = ds.columns
- )
+# ds1 = pd.DataFrame(
+#       gaussian_filter( 
+#           ds.to_numpy(), 
+#           sigma = 0.5
+#           ),
+#       index = ds.index,
+#       columns = ds.columns
+#   )
+
+ds1 = ds.copy()
  
 ax[0].contourf(
-     ds1.columns, 
-     ds1.index, 
-     ds1.values,
-     cmap = 'seismic',
-     levels = 50
-     # levels = np.linspace(-1, 1, 30)
-     )
+      ds1.columns, 
+      ds1.index, 
+      ds1.values,
+      cmap = 'seismic',
+      levels = 50
+      # levels = np.linspace(-1, 1, 30)
+      )
 
 
-# ds = wv.create_synthetic_wave_grid()
-
-
+# # ds = wv.create_synthetic_wave_grid()
